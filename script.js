4c46d4f3e7511cf07a2e20c6ed29a0f9c33c9404
EliasGomezC <eliasgcueva@gmail.com>
2025-10-03 13:23:14 -0700

    header mobile

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const btn_Header = document.querySelector('#btn-headerMobile');
 const headerPanel = document.querySelector('#header_Mobile');
 let open = false;
+
 btn_Header.addEventListener('click', () => {
     open = !open;
     headerPanel.style.maxHeight = open ? headerPanel.scrollHeight + 'px' : '0px';
     btn_Header.setAttribute('aria-expanded', String(open));
+});
+
+const headerLinks = headerPanel.querySelectorAll('a');
+headerLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        open = false;
+        headerPanel.style.maxHeight = '0px';
+        btn_Header.setAttribute('aria-expanded', 'false');
+    });
 });
 
 document.querySelector('#year').textContent = new Date().getFullYear();
